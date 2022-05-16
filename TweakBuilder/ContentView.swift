cfdd011b056bfc958edc6d403a2dd400cd5cf9ee
Matthew <anisovich.matthew@gmail.com>
2022-05-16 14:00:16 +0100

    Initial Commit

---
+++ TweakBuilder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TweakBuilder
+//
+//  Created by exerhythm on 16.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TweakBuilder/TweakBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TweakBuilderApp.swift
+//  TweakBuilder
+//
+//  Created by exerhythm on 16.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TweakBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
